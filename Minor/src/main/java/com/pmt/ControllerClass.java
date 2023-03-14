7ebdaa327a92909b17ff480470105e6b7ba745f3
Samarp1001 <samarpjain01@gmail.com>
2023-03-14 18:15:44 +0530

    added new page

--- Minor/src/main/java/com/pmt/ControllerClass.java
+++ Minor/src/main/java/com/pmt/ControllerClass.java
@@ -7,15 +7,17 @@ package com.pmt;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- *
- * @author Akash
- */
 @Controller
 public class ControllerClass {
     @RequestMapping("/welcomepage")
     public String welcomefunction(){
         System.out.println("This is welcome page");
         return "welcomepage";
+    }
+    @RequestMapping("/helloworld")
+    public String hellowrold()
+    {
+        System.out.println("this is hello world");
+        return "hell";
     }
 }
